01142cf09fdcd051269582758fbe999afdb8a165
Leonardo Ikejiri <leoikejiri@gmail.com>
2020-06-26 13:24:22 -0300

    Project Created

---
+++ src/main/java/com/example/sbfrontbackend/SbFrontBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbfrontbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFrontBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFrontBackEndApplication.class, args);
+	}
+
+}
